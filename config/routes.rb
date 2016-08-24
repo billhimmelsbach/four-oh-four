0cf5094daa9fd65a00c08e219ea463304e8c45d4
billhimmelsbach <whimmels@gmail.com>
2016-08-24 00:34:23 -0700

    tries to pin down error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Fourohfour::Engine.routes.draw do
     get "/fourohfour", to: "test#index", as: "test"
-    get "*unmatched_route", to: "error#not_found", 
+    get "*unmatched_route", to: "error#not_found",
+    
 end
